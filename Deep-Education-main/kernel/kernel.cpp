29366d4a7623f584bb8f97b2aeed76f30552ca3d
scottmarsden <90927624+scottmarsden@users.noreply.github.com>
2021-11-15 16:16:23 -0500

    Added parallel implementation and refactored

--- Deep-Education-main/kernel/kernel.cpp
+++ Deep-Education-main/kernel/kernel.cpp
@@ -3,6 +3,7 @@
 #include <limits>
 
 #include "kernel.h"
+#include "omp.h"
 
 using std::cout;
 using std::endl;
@@ -10,6 +11,33 @@ using std::endl;
 int THD_COUNT = 1;
 
 using std::string;
+
+void normalize(csr_t* snaph, array2d_t<float> & matrix) {
+  #pragma omp parallel{
+    #pragma omp for
+    for (int i = 0 ; i < matrix.row_count ; i++) {
+        matrix.row_normalize(i, snaph->get_degree(i));
+    }
+  }
+}
+
+void multiply(csr_t* snaph, array2d_t<float> & input, array2d_t<float> & output){
+    vid_t* offset = snaph->offset;
+    vid_t* nebrs = snaph->nebrs;
+    
+  
+    #pragma omp parallel {
+      #pragma omp for
+      for (int i = 0; i < output.row_count; i++) {
+         output.row_add(input.data_ptr + i*output.col_count, i); 
+        
+        for (int j = offset[i] ; j < offset[i+1] ; j++) {
+              output.row_add(input.data_ptr + nebrs[j]*output.col_count, nebrs[j]);
+          }
+      }
+    }
+  
+}
 
 
 void _gspmm(csr_t* snaph, array2d_t<float> & input, array2d_t<float> & output,
@@ -20,15 +48,26 @@ void _gspmm(csr_t* snaph, array2d_t<float> & input, array2d_t<float> & output,
     //If in backward, normalize it first, else normalize it after computation
 
     //The core logic goes here.
+  
+  
+  
+    if(reverse){
+    normalize(snaph, input);
+    multiply(snaph, input, output);
+    }
+    else{
+      normalize(snaph, input);
+      multiply(snaph, input, output);
+    }
 
-    int64_t columnCount = output.columnCount;
+    /*int64_t columnCount = output.columnCount;
     int64_t rowCount = output.rowCount;
     vid_t* nbrs = snaph->get_nebrs();
     vid_t* offset = snaph->get_offset();
     vid_t v_count = snaph->get_vcount();
 
     for(vid_t i = 0; i <v_count; i++){
-    vid_t deg = snaph->get_degree(node);
+    vid_t deg = snaph->get_degree(node)
     if(reverse == 0){
     	for (vid_t nbrID = offset[i]; nbrID < offset[i]+ deg; nbrID++){
     		output.row_add(input.operator[](nbrs[nbrID]), i);
@@ -40,8 +79,8 @@ void _gspmm(csr_t* snaph, array2d_t<float> & input, array2d_t<float> & output,
     	for (vid_t nbrID = offset[i]; nbrID < offset[i]+ deg; nbrID++){
     		output.row_add(input.operator[](nbrs[nbrID]), i);
     	}
-    }
-
+    } */
+   
 
 }
 
